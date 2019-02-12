055fdb03bb6541af674f1c7286c0891f802ab542
Sergii <sergykoretsfsp@gmail.com>
2019-02-12 17:37:56 +0200

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module DemoTape
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
+    config.assets.enabled = true
+    config.assets.version = '1.1'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
